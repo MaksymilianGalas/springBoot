79c819c0c39d187a7e475ca02370af871941b45d
MaksymilianGalas <maksemgal@gmail.com>
2023-10-26 15:49:56 +0200

    project v4.0

--- src/main/java/com/example/nowaaplikacja/data/Lectures.java
+++ src/main/java/com/example/nowaaplikacja/data/Lectures.java
@@ -3,27 +3,16 @@ package com.example.nowaaplikacja.data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@Getter
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Student {
-
+public class Lectures {
     @Id
     @GeneratedValue
     private UUID id;
     private String name;
-    private StudentUnit unit;
-    private Long index;
-
-    @OneToOne
-    private Lectures lectures;
 }
